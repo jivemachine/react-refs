46bf2df179e258c20903bfdf60dcd219d9d6a0f0
Jeremy <the.jeremy.cobb@gmail.com>
2024-03-22 15:47:51 -0500

    small changes to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-import Home from "./components/Home";
+import Home from "./pages/Home";
 import Twatter from "./pages/Twatter";
 
+
 const App = () => {
-	return (
-		<BrowserRouter>
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/twatter" element={<Twatter />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+        <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/twatter" element={<Twatter />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
