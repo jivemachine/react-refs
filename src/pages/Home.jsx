3cd3a52550ed6be4bd6e3b49e92a66b3b3e1c7ba
Jeremy <the.jeremy.cobb@gmail.com>
2024-03-22 16:38:54 -0500

    just starting on twatter and the checks page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
 const Home = () => {
 	return (
-		<div>
-			<h1>Home</h1>
-			<p>Welcome to the Home page!</p>
-		</div>
+        <main className="container">
+            <section className="row justify-content-center">
+                <div className="card shadow">
+                    <div className="card-body">
+                        <h4 className="card-title">title</h4>
+                        <p className="card-text">body</p>
+                        <button className="btn btn-outline-primary">Lookin' Good!</button>
+                        <button className="btn btn-outline-danger">NO GOOD!</button>
+                    </div>
+                </div>
+            </section>
+        </main>
 	);
 };
 
--- src/pages/Twatter.jsx
+++ src/pages/Twatter.jsx
@@ -1,8 +1,31 @@
+import { useRef, useState } from 'react';
+
 const Twatter = () => {
+    const dayjs = require('dayjs');
+    const timeJS = Date.now();
+    const timeAlreadySet = dayjs(timeJS).format('YYYY-MM-DD HH:mm:ss');
+
+
 	return (
-		<div>
-			<h1>Twatter lmao</h1>
-		</div>
+        <main className='container'>
+            <section className="row justify-content-center mt-5">
+                <div className="card shadow mb-2">
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" name="username" id="username" className='form-control' />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="message">Message</label>
+                            <textarea name="message" id="message" cols="30" rows="5"></textarea>
+                        </div>
+                        <div className="form-group">
+                            <button type="submit" className="btn btn-primary"></button>
+                        </div>
+                    </form>
+                </div>
+            </section>
+        </main>
 	);
 };
 
