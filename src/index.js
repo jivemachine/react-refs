74a14a069876886f539cde528bd95d3ad997f96c
Jeremy <the.jeremy.cobb@gmail.com>
2024-03-22 16:36:59 -0500

    lmao '<StrictNode>'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.css';
 
 import React from 'react';
-import { createRoot } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictNode>
+    <React.StrictMode>
         <App />
-    </React.StrictNode>
+    </React.StrictMode>
 );
