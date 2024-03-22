32ebd99aaaacb6f83a7e4ee3894dc3dce2cc0d3a
Jeremy <the.jeremy.cobb@gmail.com>
2024-03-22 15:31:40 -0500

    integrated navbar and routes for other pages in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Twatter from "./pages/Twatter";
+
 const App = () => {
-    return (
-        <div>
-            <h1>App</h1>
-        </div>
-    );
+	return (
+		<BrowserRouter>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/twatter" element={<Twatter />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;
